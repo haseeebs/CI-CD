7bb50982af4797867e31be9c2a18736a7a2e1fa0
haseeb hummo <haseebpersonalwork@gmail.com>
2024-04-29 15:09:39 +0530

    Switch database connection to MongoDB Atlas instead of localhost.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -12,9 +14,10 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
+// main().catch(err => console.log(err));
 
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDb connected sucessfully...'))
+    .catch(err => console.log(err));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
