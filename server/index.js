ef9db6c947f582a329b48a443e50b3ce800005e0
haseeb hummo <haseebpersonalwork@gmail.com>
2024-05-01 07:10:37 +0530

    Create CI configuration file for deployment with GitHub Actions.

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use("/user", userRouter)
 
 // main().catch(err => console.log(err));
 
-mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDb connected sucessfully...'))
+mongoose.connect(`${process.env.MONGODB_URL}`)
+    .then(() => console.log('MongoDB connected sucessfully...'))
     .catch(err => console.log(err));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
